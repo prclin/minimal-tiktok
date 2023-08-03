66a20e41cea2a8eb14821211c9f3f970d695fe26
imyttizy <1664619620@qq.com>
2023-08-03 23:32:49 +0800

    feat: add health interface

--- controller/controller.go
+++ controller/controller.go
@@ -1,8 +1,20 @@
 package controller
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/prclin/minimal-tiktok/core"
+	"github.com/prclin/minimal-tiktok/model/response"
+)
+
 /*
 Init 为了显示执行副作用引入
 */
 func Init() {
 	//just empty
+}
+
+func init() {
+	core.Router.GET("/health", func(context *gin.Context) {
+		context.JSON(200, response.Response{StatusCode: 200, StatusMsg: "ok"})
+	})
 }
