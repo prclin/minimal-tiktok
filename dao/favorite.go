d17a7a1428477e8d95522c0eac548bc8b6d41e49
imyttizy <1664619620@qq.com>
2023-08-20 00:19:18 +0800

    feat: finish post favorite action

--- dao/favorite.go
+++ dao/favorite.go
@@ -1,9 +1,23 @@
 package dao
 
-import "github.com/prclin/minimal-tiktok/global"
+import (
+	"github.com/prclin/minimal-tiktok/global"
+	"github.com/prclin/minimal-tiktok/model/entity"
+	"gorm.io/gorm"
+)
 
 func IsFavorite(userId uint64, videoId uint64) bool {
 	var favorited bool
 	global.Datasource.Raw("select count(user_id) from favorite where user_id=? and video_id=?", userId, videoId).Scan(&favorited)
 	return favorited
+}
+
+func InsertFavorite(tx *gorm.DB, favorite entity.Favorite) error {
+	sql := "insert into favorite(user_id, video_id) value (?,?)"
+	return tx.Exec(sql, favorite.UserId, favorite.VideoId).Error
+}
+
+func DeleteFavorite(tx *gorm.DB, favorite entity.Favorite) error {
+	sql := "delete from favorite where user_id=? and video_id=?"
+	return tx.Exec(sql, favorite.UserId, favorite.VideoId).Error
 }
--- dao/video.go
+++ dao/video.go
@@ -11,3 +11,10 @@ func SelectVideosByUserId(userId uint64) []entity.Video {
 	global.Datasource.Raw("select id,title,play_url,cover_url,favorite_count,comment_count,extra,create_time from video where user_id=?", userId).Scan(&videos)
 	return videos
 }
+
+func ExistsVideo(id uint64) bool {
+	var exist bool
+	sql := "select count(id) from video where id=?"
+	global.Datasource.Raw(sql, id).Scan(&exist)
+	return exist
+}
